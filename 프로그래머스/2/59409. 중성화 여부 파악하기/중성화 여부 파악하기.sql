041e3792cf7b52ae0ef9a953a8705136b29acbee
Jeong Su A <86153736+jeongsa@users.noreply.github.com>
2024-01-04 11:26:30 +0900

    [level 2] Title: 중성화 여부 파악하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/2/59409. 중성화 여부 파악하기/중성화 여부 파악하기.sql
@@ -0,0 +1,8 @@
+-- 코드를 입력하세요
+-- 보호소 동물의 중성화 여부  출력 
+SELECT ANIMAL_ID, NAME, 
+CASE WHEN SEX_UPON_INTAKE like '%Neutered%' THEN 'O'
+WHEN SEX_UPON_INTAKE like '%Spayed%' THEN 'O'
+ELSE 'X' END AS 중성화
+FROM ANIMAL_INS
+ORDER BY ANIMAl_ID;
